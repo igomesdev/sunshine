f6505708361145d4408f6f513a20b703467e3f33
Your Name <gomezz50@hotmail.com>
2021-04-03 14:45:50 +0100

    added npm packages, tailwind

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta htt-equiv="X-UA-Compatible" content="ie=edge">
         <title>SunShine Estates</title>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
     <body>
         @yield('content')    
