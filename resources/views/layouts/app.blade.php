67e3ae371f86e1c78958602b8b01e599489434c2
Your Name <gomezz50@hotmail.com>
2021-04-03 13:41:28 +0100

    added posts, layouts folders

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta htt-equiv="X-UA-Compatible" content="ie=edge">
+        <title>SunShine Estates</title>
+    </head>
+    <body>
+        @yield('content')    
+    </body>
+</html>
+
