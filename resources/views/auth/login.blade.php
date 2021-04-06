aca3e2e0158968985cf7f4379925279cbe6ee432
Your Name <gomezz50@hotmail.com>
2021-04-06 19:44:29 +0100

    added the logout functionality, added middleware into login, logout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,7 +9,6 @@
                 </div>
             @endif
 
-
             <form action="{{ route('login') }}" method="post">
             @csrf
             <div class="mb-4">
@@ -36,7 +35,6 @@
             <div>
                 <button type="submit" class="bg-blue-500 text-white px-4 py-3 rounded font-medium w-full">Login</button>
             </div>
-            
             </form>
         </div>
     </div>
