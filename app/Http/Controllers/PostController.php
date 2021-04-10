56eebd6e8e030af642bfac4d7b3e4d5933c9a7fc
Your Name <gomezz50@hotmail.com>
2021-04-10 12:24:44 +0100

    added a relationship between user and post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,17 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index() 
     {
-        return view('posts.index');
+        $posts = Post::get();
+
+        return view('posts.index',
+        [
+           'posts' => $posts 
+        ]);
     }
 
     public function store(Request $request) 
     {
-        dd('ok');
+        $this->validate($request, [
+            'body' => 'required'
+        ]);
+
+        $request->user()->posts()->create([
+            'body' => $request->body
+        ]);
+
+        return back();
     }
 }
