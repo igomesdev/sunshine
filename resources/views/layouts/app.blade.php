6bb68b84e86b01e4deaeab33fb037a1fbeec1627
Your Name <gomezz50@hotmail.com>
2021-05-08 09:12:23 +0100

    deleted the footer, moved the nav to different page, removed empty spaces

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,52 +5,23 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta htt-equiv="X-UA-Compatible" content="ie=edge">
         <title>SunShine Estates</title>
+        <!-- Bootstrap -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
+        <!-- Bundle JS -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+
         <!-- Styles -->
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <link href="{{ asset('icons/all.min.css') }}" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     </head>
     <body class="bg-gray-200">
-    <nav class="p-6 bg-white flex justify-between mb-6">
-        <ul class="flex items-center">
-            <li>
-                <a href="/" class="p-3">Home</a>
-            </li>
-            <li>
-                <a href="{{ route('dashboard') }}" class="p-3">Dashboard</a>
-            </li>
-            <li>
-                <a href="{{ route('posts') }}" class="p-3">Post</a>
-            </li>
-        </ul>
-    
-        <ul class="flex items-center">
-            @auth
-                <li>
-                    <a href="" class="p-3">{{ auth()->user()->name }}</a>
-                </li>
-                <li>
-                    <form action="{{ route('logout') }}" method="post" class="p-3 inline">
-                        @csrf
-                        <button type="submit">Log Out</button>
-                    </form>
-                </li>
-            @endauth
-
-            @guest
-                <li>
-                    <a href="{{ route('login') }}" class="p-3">Login</a>
-                </li>
-                <li>
-                    <a href="{{ route('register') }}" class="p-3">Register</a>
-                </li>
-            @endguest
-        </ul>
-    </nav>
-        @yield('content')
 
         <div class="footer">
-            @include('layouts.footer')
+            @include('layouts.navigation')
         </div> 
+    
+        @yield('content')
     </body>
 </html>
